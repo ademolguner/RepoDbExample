08ad235c4e3ff0b0a446850aa558c943dd7cc0ae
ADEM OLGUNER <ademolguner@gmail.com>
2020-04-10 00:06:49 +0300

    code review

--- RepoDbExample/RepoDbExample.DataAccess/Concrete/DbConnection/PostgreSqLConnectionDatabases/FinansDbConnectionFactory.cs
+++ RepoDbExample/RepoDbExample.DataAccess/Concrete/DbConnection/PostgreSqLConnectionDatabases/FinansDbConnectionFactory.cs
@@ -9,13 +9,12 @@ using System.Text;
 namespace RepoDbExample.DataAccess.Concrete.DbConnection.PostgreSqLConnectionDatabases
 {
     // 
-    public class FinansDbConnectionFactory : IDatabaseConnectionFactory//IDbConnection
+    public class FinansDbConnectionFactory : IDatabaseConnectionFactory
     {
         private string _connectionStringValue;
         public FinansDbConnectionFactory()
         {
             _connectionStringValue = new AppConfiguration(DatabaseConnectionName.FinansDb)._connectionString;
-            //CreateConnection();
         }
         public string ConnectionString
         {
@@ -27,63 +26,6 @@ namespace RepoDbExample.DataAccess.Concrete.DbConnection.PostgreSqLConnectionDat
         {
             return new NpgsqlConnection(_connectionStringValue);
         }
-
-
-
-
-        //private string _connectionStringValue;
-        //public FinansDbConnectionFactory()
-        //{
-        //    _connectionStringValue = new AppConfiguration(DatabaseConnectionName.FinansDb)._connectionString;
-        //}
-        //public string ConnectionString
-        //{
-        //    get { return _connectionStringValue; }
-        //    set { ConnectionString = _connectionStringValue; }
-        //}
-
-        //public int ConnectionTimeout => 10000;
-
-        //public string Database => Database;
-
-        //public ConnectionState State => State;
-
-
-
-        //public IDbTransaction BeginTransaction()
-        //{
-        //    return BeginTransaction();
-        //}
-
-        //public IDbTransaction BeginTransaction(IsolationLevel il)
-        //{
-        //    return BeginTransaction(il);
-        //}
-
-        //public void ChangeDatabase(string databaseName)
-        //{
-        //    ChangeDatabase(databaseName);
-        //}
-
-        //public void Close()
-        //{
-        //    Close();
-        //}
-
-        //public IDbCommand CreateCommand()
-        //{
-        //    return CreateCommand();
-        //}
-
-        //public void Dispose()
-        //{
-        //    Dispose();
-        //}
-
-        //public void Open()
-        //{
-        //    Open();
-        //}
-
+         
     }
 }
--- RepoDbExample/RepoDbExample.DataAccess/Concrete/DbConnection/SqlConnectionDatabases/AoBlogDbConnectionFactory.cs
+++ RepoDbExample/RepoDbExample.DataAccess/Concrete/DbConnection/SqlConnectionDatabases/AoBlogDbConnectionFactory.cs
@@ -6,14 +6,13 @@ using System.Data.SqlClient;
 
 namespace RepoDbExample.DataAccess.Concrete.DbConnection.SqlConnectionDatabases
 {
-    public class AoBlogDbConnectionFactory : IDatabaseConnectionFactory//IDbConnection
+    public class AoBlogDbConnectionFactory : IDatabaseConnectionFactory
     {
 
         private string _connectionStringValue;
         public AoBlogDbConnectionFactory()
         {
             _connectionStringValue = new AppConfiguration(DatabaseConnectionName.AdemBlogDb)._connectionString;
-            //CreateConnection();
         }
         public string ConnectionString
         {
@@ -26,59 +25,6 @@ namespace RepoDbExample.DataAccess.Concrete.DbConnection.SqlConnectionDatabases
             return new SqlConnection(_connectionStringValue);
         }
 
-        //private string _connectionStringValue;
-        //public AoBlogDbConnectionFactory()
-        //{
-        //    _connectionStringValue = new AppConfiguration(DatabaseConnectionName.AdemBlogDb)._connectionString;
-        //}
-
-        //public string ConnectionString
-        //{
-        //    get { return _connectionStringValue; }
-        //    set { ConnectionString = _connectionStringValue; }
-        //}
-
-        //public int ConnectionTimeout => 10000;
-
-        //public string Database => Database;
-
-        //public ConnectionState State => State;
-
-
-
-        //public IDbTransaction BeginTransaction()
-        //{
-        //    return BeginTransaction();
-        //}
-
-        //public IDbTransaction BeginTransaction(IsolationLevel il)
-        //{
-        //    return BeginTransaction(il);
-        //}
-
-        //public void ChangeDatabase(string databaseName)
-        //{
-        //    ChangeDatabase(databaseName);
-        //}
-
-        //public void Close()
-        //{
-        //    Close();
-        //}
-
-        //public IDbCommand CreateCommand()
-        //{
-        //    return CreateCommand();
-        //}
-
-        //public void Dispose()
-        //{
-        //    Dispose();
-        //}
-
-        //public void Open()
-        //{
-        //    Open();
-        //}
+        
     }
 }
